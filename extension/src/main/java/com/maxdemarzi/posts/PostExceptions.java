e93b393a1cdc2c47fd900e453cb8d92d0aa7b42c
Max De Marzi <maxdemarzi@hotmail.com>
2018-04-29 23:38:32 -0500

    making Post Exceptions public

--- extension/src/main/java/com/maxdemarzi/posts/PostExceptions.java
+++ extension/src/main/java/com/maxdemarzi/posts/PostExceptions.java
@@ -2,11 +2,11 @@ package com.maxdemarzi.posts;
 
 import com.maxdemarzi.Exceptions;
 
- class PostExceptions extends Exceptions {
+ public class PostExceptions extends Exceptions {
 
      static final Exceptions missingStatusParameter = new Exceptions(400, "Missing status Parameter.");
      static final Exceptions emptyStatusParameter = new Exceptions(400, "Empty status Parameter.");
-     static final Exceptions postNotFound = new Exceptions(400, "Post not Found.");
+     public static final Exceptions postNotFound = new Exceptions(400, "Post not Found.");
 
      PostExceptions(int code, String error) {
         super(code, error);
