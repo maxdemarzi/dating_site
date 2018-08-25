1bc8102c4f7d05c0616ff77a08c602ac47410c02
Max De Marzi <maxdemarzi@hotmail.com>
2018-08-25 09:43:32 -0500

    do not allow anonymous to see messages

--- website/src/main/java/com/maxdemarzi/routes/Messages.java
+++ website/src/main/java/com/maxdemarzi/routes/Messages.java
@@ -6,7 +6,6 @@ import org.jooby.*;
 import org.pac4j.core.profile.CommonProfile;
 import retrofit2.Response;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class Messages  extends Jooby {
@@ -15,22 +14,16 @@ public class Messages  extends Jooby {
     }
     {
         get("/messages", req -> {
-            String requested_by = req.get("requested_by");
-            if (requested_by.equals("anonymous")) requested_by = null;
-            User authenticated = App.getUserProfile(requested_by);
+            CommonProfile profile = require(CommonProfile.class);
+            String username = profile.getUsername();
+            User authenticated = App.getUserProfile(username);
 
-            Response<User> userResponse = App.api.getProfile(requested_by, null).execute();
-            if (userResponse.isSuccessful()) {
-                User user = userResponse.body();
-                Integer limit = req.param("limit").intValue(25);
-                Integer offset = req.param("offset").intValue(0);
+            Integer limit = req.param("limit").intValue(25);
+            Integer offset = req.param("offset").intValue(0);
 
-                Response<List<Conversation>> conversationResponse = App.api.getConversations(user.getUsername()).execute();
-                List<Conversation> conversations = new ArrayList<>();
-                if (conversationResponse.isSuccessful()) {
-                    conversations = conversationResponse.body();
-                }
-
+            Response<List<Conversation>> conversationResponse = App.api.getConversations(username).execute();
+            if (conversationResponse.isSuccessful()) {
+                List<Conversation> conversations = conversationResponse.body();
                 return views.messages.template(authenticated, conversations);
             } else {
                 throw new Err(Status.BAD_REQUEST);
@@ -38,27 +31,18 @@ public class Messages  extends Jooby {
         });
 
         get("/messages/{username}", req -> {
-            String requested_by = req.get("requested_by");
-            if (requested_by.equals("anonymous")) requested_by = null;
-            User authenticated = App.getUserProfile(requested_by);
+            CommonProfile profile = require(CommonProfile.class);
+            String username = profile.getUsername();
+            User authenticated = App.getUserProfile(username);
+            User user2 = App.getUserProfile(req.param("username").value());
 
-            Response<User> userResponse = App.api.getProfile(requested_by, null).execute();
-            Response<User> user2Response = App.api.getProfile(req.param("username").value(), null).execute();
-            if (userResponse.isSuccessful() && user2Response.isSuccessful()) {
-                User user = userResponse.body();
-                User user2 = user2Response.body();
+            Integer limit = req.param("limit").intValue(25);
+            Integer offset = req.param("offset").intValue(0);
 
-                Integer limit = req.param("limit").intValue(25);
-                Integer offset = req.param("offset").intValue(0);
-
-                Response<List<Message>> conversationResponse = App.api.getConversation(user.getUsername(), user2.getUsername()).execute();
-                List<Message> messages = new ArrayList<>();
-                if (conversationResponse.isSuccessful()) {
-                    messages = conversationResponse.body();
-                }
-
+            Response<List<Message>> conversationResponse = App.api.getConversation(username, req.param("username").value()).execute();
+            if (conversationResponse.isSuccessful()) {
+                List<Message> messages = conversationResponse.body();
                 return views.conversation.template(authenticated, user2, messages);
-
             } else {
                 throw new Err(Status.BAD_REQUEST);
             }
